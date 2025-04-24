7cf938d6f2fd6a3cff1a245df10a661a5ed90cef
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-04-24 17:33:07 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,29 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const values = [
+    person.name,
+    person.sex === 'm' ? 'Male' : 'Female',
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
+
+  values.forEach((value) => {
+    const cell = document.createElement('td');
+
+    cell.textContent = value;
+    row.appendChild(cell);
+  });
+
+  table.appendChild(row);
+});
